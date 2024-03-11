f783d73081a8d1deac39df88cf793a9eacb5c4f2
NiTro005 <nitro2005002@gmail.com>
2024-03-11 14:53:00 +0300

    fixed bugs

--- OOP-projects/CTime/ctime.cpp
+++ OOP-projects/CTime/ctime.cpp
@@ -1,18 +1,6 @@
 #include "ctime.h"
 #include <iostream>
 
-Status check();
-Status convert();
-void output();
-void assign(CTime& time);
-void set_values(int hours_, int minutes_, int seconds_);
-Status add_hours(int hours_);
-Status add_minutes(int minutes_);
-Status add_seconds(int minutes_);
-Status sub_hours(int hours_);
-Status sub_minutes(int minutes_);
-Status sub_seconds(int minutes_);
-int compare(const CTime& time);
 
 CTime::CTime(){
 	int hours = 0;
