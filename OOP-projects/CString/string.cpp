f2a9b12acad10e3238bcf81362f4536ebe9cbb64
NiTro005 <nitro2005002@gmail.com>
2024-03-12 17:50:16 +0300

    constructor

---
+++ OOP-projects/CString/string.cpp
@@ -0,0 +1,153 @@
+#include "string.h"
+
+/// <summary>
+/// Конструктор по умолчанию.
+/// </summary>
+CString::CString() {
+    _size = 0;
+    _capacity = STEP_CAPACITY;
+    _data = new char[_capacity];
+    _data[0] = '\0';
+}
+
+/// <summary>
+/// Конструктор копирования.
+/// </summary>
+/// <param name="str"> - копируемая строка</param>
+CString::CString(const CString& str) {
+    _size = str._size;
+    _capacity = str._capacity;
+    _data = new char[_capacity];
+    for (size_t i = 0; i < _size; i++) {
+        _data[i] = str._data[i];
+    }
+    _data[_size] = '\0';
+}
+
+/// <summary>
+/// Деструктор.
+/// </summary>
+
+CString::CString(const char* c_str) {
+    _size = 0;
+    while (c_str[_size] != '\0') {
+        _size++;
+    }
+    _capacity = (_size / STEP_CAPACITY) * STEP_CAPACITY + STEP_CAPACITY;
+    _data = new char[_capacity];
+    for (int i = 0; i < _size; i++) {
+        _data[i] = c_str[i];
+    }
+    _data[_size] = '\0';
+}
+
+CString::~CString() {
+    delete[] _data;
+    _data = nullptr;
+}
+
+/// <summary>
+/// Проверка строки на пустоту.
+/// </summary>
+/// <returns>
+/// true - если строка пуста,
+/// false - иначе.
+/// </returns>
+bool CString::empty() const noexcept {
+    return _size == 0;
+}
+
+/// <summary>
+/// Геттер для размера строки.
+/// </summary>
+/// <returns>размер (длина) строки</returns>
+size_t CString::size() const noexcept {
+    return _size;
+}
+/// <summary>
+/// Функция сравнения (лексикографического).
+/// </summary>
+/// <param name="str"> - строка для сравнения</param>
+/// <returns>
+/// 1 - если исходная строка больше,
+/// -1 - если исходная строка меньше,
+/// 0 - если строки совпадают.
+/// </returns>
+int CString::compare(const CString& str) const noexcept {
+    for (size_t i = 0; i < algorithms::min(_size, str._size); i++) {
+        if (this->_data[i] < str._data[i]) {
+            return 1;
+        }
+        else if (this->_data[i] > str._data[i]) {
+            return -1;
+        }
+    }
+    if (this->_size > str._size) return 1;
+    else if (this->_size < str._size) return -1;
+    else return 0;
+}
+
+/*
+/// <summary>
+/// Вставка символа в конец строки.
+/// </summary>
+/// <param name="c"> - символ для вставки</param>
+void CString::push_back(char c) {
+    if (this->full()) {
+        this->reserve(_capacity);
+    }
+
+    _data[_size] = c;
+    _data[++_size] = '\0';
+}*/
+
+/// <summary>
+/// Удаление символа из конца строки.
+/// <exception cref="std::logic_error">Исключение, если исходная строка пуста.</exception>
+/// </summary>
+void CString::pop_back() {
+    if (this->empty()) {
+        throw std::logic_error("Error in function \
+                  \"void pop_back()\": source CString is empty");
+    }
+    _data[--_size] = '\0';
+}
+
+/*
+// лишь пример реализации
+/// <summary>
+/// Вставка строки после заданной позиции в исходной.
+/// </summary>
+/// <param name="pos"> - позиция для вставки в исходной строке.</param>
+/// <param name="str"> - строка для вставки</param>
+/// <returns>изменённая исходная строка</returns>
+CString& CString::insert(size_t pos, const CString& str) {
+    CString begin(*this, 0, pos);
+    CString end(*this, pos, _size - pos);
+    begin.append(str);
+    begin.append(end);
+    this->swap(begin);
+    return *this;
+}*/
+
+/// <summary>
+/// Поиск первого совпадения в исходной строке с одним из символов заданной строки.
+/// </summary>
+/// <param name="str"> - набор символов для поиска</param>
+/// <param name="pos"> - позиция, с которой следует начинать поиск</param>
+/// <returns></returns>
+size_t CString::find_first_of(const CString& str, size_t pos) const {
+    for (size_t i = pos; i < _size; i++) {
+        for (size_t j = 0; j < str._size; j++) {
+            if (_data[i] == str._data[j]) {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+void CString::print() const noexcept {
+    for (int i = 0; i < _size; i++) {
+        std::cout << _data[i];
+    }
+}
