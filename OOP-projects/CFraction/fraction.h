68094ed5d428491ac877c81578452648788c2c04
NiTro005 <nitro2005002@gmail.com>
2024-04-14 23:57:43 +0300

    file spliting

---
+++ OOP-projects/CFraction/fraction.h
@@ -0,0 +1,64 @@
+#pragma once
+#include <iostream>
+class Fraction {
+    int numerator;       // числитель
+    int denominator;     // заменатель
+public:
+    Fraction() : numerator(0), denominator(1) {}
+    Fraction(int num, int dem) : numerator(num), denominator(dem) {}
+    Fraction(const Fraction& frac) : numerator(frac.numerator), denominator(frac.denominator) {}
+
+    Fraction operator+(const Fraction& frac);
+    Fraction operator-(const Fraction& frac);
+    Fraction operator*(const Fraction& frac);
+    Fraction operator/(const Fraction& frac);
+    Fraction operator+=(const Fraction& frac);
+    Fraction operator-=(const Fraction& frac);
+    Fraction operator*=(const Fraction& frac);
+    Fraction operator/=(const Fraction& frac);
+    Fraction operator=(const Fraction& frac);
+
+    bool operator==(const Fraction& frac) const;
+    bool operator>=(const Fraction& frac) const;
+    bool operator<=(const Fraction& frac) const;
+    bool operator>(const Fraction& frac) const;
+    bool operator<(const Fraction& frac) const;
+    bool operator!=(const Fraction& frac) const;
+
+    friend Fraction operator+(Fraction& frac, int value);
+    friend Fraction operator-(Fraction& frac, int value);
+    friend Fraction operator*(Fraction& frac, int value);
+    friend Fraction operator/(Fraction& frac, int value);
+    friend Fraction operator+=(Fraction& frac, int value);
+    friend Fraction operator-=(Fraction& frac, int value);
+    friend Fraction operator*=(Fraction& frac, int value);
+    friend Fraction operator/=(Fraction& frac, int value);
+    friend Fraction operator+(int value, Fraction& frac);
+    friend Fraction operator-(int value, Fraction& frac);
+    friend Fraction operator*(int value, Fraction& frac);
+    friend Fraction operator/(int value, Fraction& frac);
+    friend Fraction operator+=(int value, Fraction& frac);
+    friend Fraction operator-=(int value, Fraction& frac);
+    friend Fraction operator*=(int value, Fraction& frac);
+    friend Fraction operator/=(int value, Fraction& frac);
+
+    friend bool operator==(const Fraction& frac, int value);
+    friend bool operator==(int value, const Fraction& frac);
+    friend bool operator>=(const Fraction& frac, int value);
+    friend bool operator>=(int value, const Fraction& frac);
+    friend bool operator<=(const Fraction& frac, int value);
+    friend bool operator<=(int value, const Fraction& frac);
+    friend bool operator!=(const Fraction& frac, int value);
+    friend bool operator!=(int value, const Fraction& frac);
+    friend bool operator>(const Fraction& frac, int value);
+    friend bool operator>(int value, const Fraction& frac);
+    friend bool operator<(const Fraction& frac, int value);
+    friend bool operator<(int value, const Fraction& frac);
+
+    friend std::ostream& operator<<(std::ostream& out, const Fraction& frac);
+    friend std::istream& operator>>(std::istream& in, Fraction& frac);
+
+private:
+    int gcd(int a, int b);
+    void simplify();
+};
---
+++ OOP-projects/CFraction/main.cpp
@@ -0,0 +1,20 @@
+#include "fraction.h"
+
+int main() {
+    Fraction fr_1, fr_2(3, 4), fr_3(5, 6), fr_4(fr_2), fr_5;
+    fr_1 = fr_2 + fr_3;
+    std::cout << fr_1 << std::endl;
+    std::cin >> fr_5;
+    fr_5 /= 3;
+    std::cout << fr_5 << std::endl;
+    if (fr_2 == fr_4) {
+        std::cout << "TRUE" << std::endl;
+    }
+    else { std::cout << "FALSE" << std::endl; }
+
+    if (fr_2 > 1) {
+        std::cout << "TRUE" << std::endl;
+    }
+    else { std::cout << "FALSE" << std::endl; }
+    return 0;
+}
