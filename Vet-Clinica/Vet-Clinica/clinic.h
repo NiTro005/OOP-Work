b5a2c255bae5ade7174c82a2d4266f208091c433
NiTro005 <nitro2005002@gmail.com>
2024-04-22 00:42:52 +0300

    settings1

---
+++ Vet-Clinica/Vet-Clinica/clinic.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+#include <archive.h>
+
+
+class FIO {
+	std::string sourname;
+	std::string name;
+	std::string patronymic;
+};
+
+
+class Person {
+	FIO full_name;
+	
+};
+
+
+
