edb097bf443363584237bc4dddee19741ef6cddb
NiTro005 <nitro2005002@gmail.com>
2024-04-22 23:46:42 +0300

    realise template vet clinica

--- Vet-Clinica/Vet-Clinica/clinic.h
+++ Vet-Clinica/Vet-Clinica/clinic.h
@@ -1,20 +1,19 @@
 #pragma once
-
-#include <string>
+#include <iostream>
+#include <string.h>
 #include <archive.h>
 #include <ctime.h>
 #include <cdate.h>
 
 enum Gender { Male, Female };
+enum Specialize {unknow, surgion, ornithologist, laboratory_assistant, therapist, surgeon, ultrasound_diagnostician, anesthetist, dermatologist, cardiologist };
+enum IlnessTip {Cancer, Obesity, Fractures, Heartworm, Fleas_and_ticks, Diarrhea, Cough, Ear_infections, Arthritis, Cataracts};
+enum Species { cat, dog, snake, parrot, rat, pig };
 
 class FIO {
-	std::string sourname;
-	std::string name;
-	std::string patronymic;
-};
-
-class Adress {
-
+	CString firstname;
+	CString sourname;
+	CString patronymic;
 };
 
 class Person {
@@ -22,6 +21,69 @@ class Person {
 	CDate birthday;
 	Gender gender;
 };
+class Adress {
+	CString city;
+	CString street;
+	CString home;
+};
 
+class Passport {
+	int number;
+	int seria;
+	Adress registration;
+};
 
+class Client : Person {
+	Passport passport;
+	CString email;
+	CString phone;
+	Adress adress;
+public:
+	void change_phone(CString& phone);
 
+};
+
+class OpenHours {
+	CTime startwork;
+	CTime endwork;
+	CTime dinner;
+};
+
+class Doctor : Person {
+	Specialize job;
+	int StageOfWork;
+	OpenHours timework;
+public:
+	void diagnoz(Ilness& i);
+	void prescribe_treatment(Ilness& i);
+};
+
+class Pet {
+	CString name;
+	int age;
+	Species spec;
+	Client* master;
+	Ilness ilness;
+};
+
+class Ilness {
+	IlnessTip ilness;
+	CString treatment;
+	CString symptoms;
+};
+
+class Visit {
+	bool followUpNeeded;
+	CString notes;
+	Doctor* doc;
+	Pet* pet;
+public:
+	void operation(Pet& p);
+};
+
+class Story_visits : Visit {
+	TArchive <Visit> visits;
+public:
+	void add_visits_history(Visit& v);
+	const TArchive <Visit> getVisit();
+};
