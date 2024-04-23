5852cb64f295dd2f5839066851cb000593589571
NiTro005 <nitro2005002@gmail.com>
2024-04-23 22:38:49 +0300

    Implementation of Vizzard Class Defense and Attack Methods

--- laba Game/Game/vizzard.cpp
+++ laba Game/Game/vizzard.cpp
@@ -5,16 +5,50 @@ Vizzard::Vizzard(std::string name, int hp, int power, int armor, int amount_of_m
 Vizzard::Vizzard(const Vizzard& c) : Character(c._name, c._hp, c._power, c._armor), amount_of_mana(c.amount_of_mana), mana(c.mana), magical(c.magical) {};
 
 int Vizzard::attack(Character& c) {
+	int damage = _power;
 	srand(time(NULL));
 	magical = (Magic)rand() % 7;
-
-	return _power;
+	damage += UseMagic(c, magical);
+	mana += 10;
+	return damage;
 }
 
 void Vizzard::defence(Character& c, int damage) {
-	
+	if (magical = Magic::shield) {
+		return;
+	}
+	if (_armor > 0) {
+		_armor -= damage;
+		if (_armor < 0) {
+			_hp += _armor; _armor = 0;
+		}
+	}
+	else { _hp -= damage; }
 }
 
 int Vizzard::UseMagic(Character& c, Magic magic) {
-
+	int damage = 0;
+	if (mana > 25) {
+		if (magic == Magic::fire) {
+			damage = _power * 2;
+		}
+		if (magic == Magic::heal) {
+			_hp += 20;
+		}
+		if (magic == Magic::lighting) {
+			damage = 10;
+			c.set_power(0);
+		}
+		if (magic == Magic::mana_bust) {
+			mana += 60;
+		}
+		if (magic == Magic::poison) {
+			damage = 10;
+			if (c.get_power() >= 5) {
+				c.set_power(c.get_power() - 2);
+			}
+		}
+	}
+	else { magic == Magic::empty; }
+	return damage;
 }
