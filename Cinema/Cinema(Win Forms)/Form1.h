41583df82bde6460d25300c5a98d7d4aaf65a776
NiTro005 <nitro2005002@gmail.com>
2024-05-10 21:15:31 +0300

    authorization window design

--- Cinema/Cinema(Win Forms)/Form1.h
+++ Cinema/Cinema(Win Forms)/Form1.h
@@ -18,6 +18,7 @@ namespace CppCLRWinFormsProject {
 		Form1(void)
 		{
 			InitializeComponent();
+			textBox2->PasswordChar = '*';
 			//
 			//TODO: Add the constructor code here
 			//
@@ -35,6 +36,12 @@ namespace CppCLRWinFormsProject {
 			}
 		}
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::CheckBox^ checkBox1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
 	protected:
 
 	private:
@@ -51,32 +58,109 @@ namespace CppCLRWinFormsProject {
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(110, 279);
+			this->button1->Location = System::Drawing::Point(97, 297);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(153, 32);
+			this->button1->Size = System::Drawing::Size(134, 34);
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"Войти, как гость";
 			this->button1->UseVisualStyleBackColor = true;
 			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(274, 297);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(141, 34);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Войти, как админ";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->ForeColor = System::Drawing::SystemColors::ButtonFace;
+			this->checkBox1->Location = System::Drawing::Point(160, 247);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(221, 21);
+			this->checkBox1->TabIndex = 2;
+			this->checkBox1->Text = L"Запомнить на этом устройстве";
+			this->checkBox1->UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(157, 101);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(208, 25);
+			this->textBox1->TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(157, 166);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(208, 25);
+			this->textBox2->TabIndex = 4;
+			this->textBox2->TextChanged += gcnew System::EventHandler(this, &Form1::textBox2_TextChanged);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
+			this->label1->Location = System::Drawing::Point(92, 103);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(59, 20);
+			this->label1->TabIndex = 5;
+			this->label1->Text = L"Логин";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->ForeColor = System::Drawing::SystemColors::ButtonHighlight;
+			this->label2->Location = System::Drawing::Point(88, 166);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(72, 20);
+			this->label2->TabIndex = 6;
+			this->label2->Text = L"Пароль";
+			// 
 			// Form1
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(7, 17);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->BackColor = System::Drawing::SystemColors::ControlDark;
-			this->ClientSize = System::Drawing::Size(599, 424);
+			this->BackColor = System::Drawing::SystemColors::InactiveCaption;
+			this->ClientSize = System::Drawing::Size(507, 395);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->checkBox1);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
+			this->Font = (gcnew System::Drawing::Font(L"Malgun Gothic", 7.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
 			this->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedDialog;
 			this->ImeMode = System::Windows::Forms::ImeMode::Off;
 			this->Name = L"Form1";
 			this->Text = L"Авторизация";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+	private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+
+	}
+};
 }
