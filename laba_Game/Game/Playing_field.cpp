f736f118368c28209d9d4c799b0b9b2de644df13
NiTro005 <nitro2005002@gmail.com>
2024-05-14 11:41:46 +0300

    constuctor playing_field

--- laba_Game/Game/Playing_field.cpp
+++ laba_Game/Game/Playing_field.cpp
@@ -1,6 +1,6 @@
 #include "character.h"
 
-Playing_field::Playing_field(int width, int height) {
+Playing_field::Playing_field(int width, int height, Game_element* player1, Game_element* player2) {
     field.resize(height);
     for (int i = 0; i < height; i++) {
         field[i].resize(width);
@@ -8,8 +8,20 @@ Playing_field::Playing_field(int width, int height) {
             field[i][j] = nullptr;
         }
     }
-    // Создаем стартовое игровое поле со случайным расположением персонажей и объектов
-    // ...
+    int i = 0, j = 0;
+    int element_count = (width * height) * 0.10;
+    for (int i = 0; i < height ; i++) {
+        bool flag = false;
+        if (i * width >= element_count) { break; }
+        for (int j = 0; j < width; j++) {
+            if (i * width + j >= element_count) { flag = true; break; }
+            field[i][j] = nullptr;  //заменить на рандомный элемент
+        }
+        if (flag) { break; }
+    }
+    field[i][j] = player1;
+    if (j + 1 < width) { field[i][j + 1] = player2; }
+    else { field[i + 1][0] = player2; }
 }
 
 void Playing_field::set_character(Character* character) {
